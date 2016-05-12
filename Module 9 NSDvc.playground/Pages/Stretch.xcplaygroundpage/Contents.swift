a8fe3b151cf4b8971716a8bee66cdc34bc433dcc
Tyler Robinson <tyler.robinson.3@gmail.com>
2016-05-12 10:21:57 -0600

    Module 9 with Correct Answer

--- Module 9 NSDvc.playground/Pages/Stretch.xcplaygroundpage/Contents.swift
+++ Module 9 NSDvc.playground/Pages/Stretch.xcplaygroundpage/Contents.swift
@@ -7,49 +7,75 @@ var str = "Hello, playground"
 //: [Next](@next)
 
 
+ let workingDicitonary = ["name" : "Derek", "age": 28, "favMovie" : "Zoolander"]
+ let brokenDictionary = ["name" : "Steve", "age": 12]
+
 
 class Person {
     
-    private let nameKey = "name"
-    private let ageKey = "age"
-    private let favMovieKey = "favMovie"
+    let nameKey = "name"
+    let ageKey = "age"
+    let favMovieKey = "favMovie"
     
-    let name: String
-    let age: Int
-    let favMovie: String
+    var name: String?
+    var age: Int?
+    var favMovie: String?
     
-    var fullDescription: String{
-    return "\(name) \(age) \(favMovie)"
-    }
     
-    var  dictionaryRepresentaion: [String: AnyObject] {
-        
-        let personDictionary: [String: AnyObject] = [nameKey: self.name, ageKey: self.age, favMovieKey: self.favMovie]
-        
-        return personDictionary
-    }
     
-    init(name: String, age: Int, favMovie: String) {
-    self.name = name
-    self.age = age
-    self.favMovie = favMovie
-    }
+     init?(dictionary: [String:AnyObject]) {
+     guard let name = dictionary[nameKey] as? String,
+     let age = dictionary[ageKey] as? Int,
+     let favMovie = dictionary[favMovieKey] as? String else {
+     print ("Failed to Initilize")
+     return nil
+     }
+      self.name = name
+      self.age = age
+      self.favMovie = favMovie
+      }
+  }
 
-    init?(dictionary: [String: AnyObject]) {
-        guard let name = dictionary[nameKey] as? String
-        let age = dictionary[ageKey] as? Int
-        let favMovie = dictionary[favMovieKey] as? String else {
-            
-            self.name = ""
-            self.age = ""
-            self.favMovie = ""
-        }
-    
-    
+let personOne = Person(dictionary: workingDicitonary)
+let personTwo = Person(dictionary: brokenDictionary)
 
-}
+//    let somePerson = Person(dictionary: workingDictionary)
+//    let anotherPreson = Person(
+//
+//    var fullDescription: String{
+//    return "\(name) \(age) \(favMovie)"
+//    }
+//    
+//    var  dictionaryRepresentaion: [String: AnyObject] {
+//        
+//        let personDictionary: [String: AnyObject] = [nameKey: self.name, ageKey: self.age, favMovieKey: self.favMovie]
+//        
+//        return personDictionary
+//    }
+//    
+//    init(name: String, age: Int, favMovie: String) {
+//    self.name = name
+//    self.age = age
+//    self.favMovie = favMovie
+//    }
+//
+//    init?(dictionary: [String: AnyObject]) {
+//        guard let name = dictionary[nameKey] as? String
+//        let age = dictionary[ageKey] as? Int
+//        let favMovie = dictionary[favMovieKey] as? String else {
+//            
+//            // If it's not there
+//            self.name = ""
+//            self.age = 0
+//            self.favMovie = ""
+//        }
+//        
+//        // If it is there
+//        
+//        self.name = name
+//        self.age = age
+//        self.favMovie = favMovie
+//}
 
-let thisPerson = Person(name: "Fred", age: 34, favMovie: "Talledaga Nights")
-print(thisPerson.dictionaryRepresentaion)
 
 
